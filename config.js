8f877bbdccc25102344c7fb5b28d4ab085e68ac5
Dana Marin <d_maringarcia@fanshaweonline.ca>
2019-12-08 02:05:33 -0500

    formatting favicon

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     host: 'cleardb.net',
     user: 'b11aaca02a8515',
     password: '4a7fd8fb', // blank for windows
-    database: 'heroku_3f4e337359923d2?',
+    database: 'heroku_3f4e337359923d2',
     port: 8889 // 3306 for windows or newer macs
 }
 
