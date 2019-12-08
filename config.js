122bbcfc38b024de41dce2dc576b8a5452901326
Dana Marin <d_maringarcia@fanshaweonline.ca>
2019-12-08 14:33:58 -0500

    DB Issue

--- config.js
+++ config.js
@@ -2,8 +2,7 @@ const config = {
     host: 'cleardb.net',
     user: 'b11aaca02a8515',
     password: '4a7fd8fb', // blank for windows
-    database: 'heroku_3f4e337359923d2',
-    port: 8889 // 3306 for windows or newer macs
+    database: 'heroku_3f4e337359923d2?',
 }
 
 module.exports = config;
